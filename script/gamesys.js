ccb0576a6dd93a0f02590bf9ca952b49663cab5a
GuardiaoX <daniel.pc.cunha@gmail.com>
2025-10-02 10:56:37 -0300

    Algo

---
+++ script/gamesys.js
@@ -0,0 +1,15 @@
+let kanji = {
+    shape : "国",
+    radicals : ["丶","囗","王"]
+    
+}
+
+console.log("Olá mundo");
+
+/** @type {HTMLDivElement} */
+const kanjiH1 = document.querySelector("#kanjiH1");
+makeQuizz();
+
+function makeQuizz (k) {
+    kanjiH1.style.backgroundColor
+}
