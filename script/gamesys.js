105f7782d932b043fffc1f42fd1d14e41b9ca93d
GuardiaoX <daniel.pc.cunha@gmail.com>
2025-10-02 16:53:50 -0300

    Adição das mecanicas

--- script/gamesys.js
+++ script/gamesys.js
@@ -1,30 +1,60 @@
+const botoes = document.querySelectorAll(".option")
+let ingamekanji
+
 const kanji = {
     id : 0,
     shape : "国",
-    radicals : ["囗","王","丶"]
+    radicals : ["囗","王","丶"],
+    lookalike : ["⼀","⼁","⺃","⺅","⼍","⼕", "⼯"]
 }
 const kanji1 = {
     id : 1,
     shape : "右",
-    radicals : ["一","ノ","口"]
+    radicals : ["一","ノ","口"],
+    lookalike : ["⺃","⺅","⼍","⼕", "⼯","⼺","⺘"]
 }
 const kanji2 = {
     id : 2,
     shape : "円",
-    radicals : ["一","｜","亠","冂"]
+    radicals : ["一","｜","亠","冂"],
+    lookalike : ["⺃","⺅","⼍","⼕", "⼯","⼺"]
 }
 
-kanjiList = [kanji, kanji1, kanji2]
+let kanjiList = [kanji, kanji1, kanji2]
 
 bushuRace()
 
-function r(max) {
-  return Math.floor(Math.random() * max);
-}
+botoes.forEach(e => e.addEventListener("click",a => selectOption(e)));
 
 function bushuRace(){
-    console.log(kanjiList[r(3)])
+    ingamekanji = kanjiList[0]
+    let options = ingamekanji.radicals.concat(ingamekanji.lookalike)
+    botoes.forEach((e,i) => e.innerHTML = options[i])
+    
 }
+
+function selectOption(button){
+    console.log(button)
+    if(ingamekanji.radicals.includes(button.innerHTML)){
+        ingamekanji.radicals.includes(button.innerHTML)
+        button.color = "red"
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 function radicalCheck(kanji, radical) {
@@ -33,4 +63,9 @@ function radicalCheck(kanji, radical) {
     }else{
         return false
     }
+}
+
+
+function r(max) {
+  return Math.floor(Math.random() * max);
 }
